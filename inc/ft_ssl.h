2e91d05243e9e38e5a1da0afcadc725e2d934790
moulin-louis <louis.moulin@outlook.fr>
2023-12-18 15:01:25 +0100

    add first files

--- inc/ft_ssl.h
+++ inc/ft_ssl.h
@@ -5,4 +5,11 @@
 #ifndef FT_SSL_H
 #define FT_SSL_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdint.h>
+
 #endif //FT_SSL_H
--- src/main.c
+++ src/main.c
@@ -1,3 +1,17 @@
 //
 // Created by loumouli on 12/18/23.
 //
+
+#include "ft_ssl.h"
+
+static void display_usage(void) {
+  fprintf(stderr, "usage: ft_ssl command [flags] [file/string]\n");
+}
+
+int main(int ac, char **av) {
+  if (ac == 1) {
+    display_usage();
+    return 1;
+  }
+  return 0;
+}
